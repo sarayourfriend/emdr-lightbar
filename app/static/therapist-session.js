c4014d111fed1732ec2aca52322e690298041051
sarayourfriend <sarayourfriend@fastmail.com>
2021-03-13 15:06:35 -0800

    Use esbuild to build JS and CSS

--- app/static/therapist-session.js
+++ app/static/therapist-session.js
@@ -1,26 +1,34 @@
-(function() {
-    const socket = io();
-	const lb = new Lightbar(document.getElementById('light-container'), false);
-	const lbc = new TherapistLightbarController(
-		lb,
-        false,
-        socket,
-		document.querySelector('[name="light-width"]'),
-		document.querySelector('[name="light-speed"]'),
-		document.querySelector('[name="light-start"]'),
-        document.getElementById('light-controls')
-	);
+import io from 'socket.io-client';
+import TherapistLightbarController from './lib/TherapistLightbarController';
+import Lightbar from './lib/Lightbar';
+import Audiobar from './lib/Audiobar';
+import TherapistAudiobarController from './lib/TherapistAudiobarController';
+import TherapistMethodController from './lib/TherapistMethodController';
+import './lib/toggleTheme';
+import './lib/Clipboard';
+import './therapist-session.css';
 
-    const ab = new Audiobar(document.getElementById('audio-container'), true);
-    const abc = new TherapistAudiobarController(
-        ab,
-        true,
-        socket,
-        document.getElementById('audio-pitch-container'),
-        document.querySelector('[name="audio-speed"]'),
-        document.querySelector('[name="audio-start"]'),
-        document.getElementById('audio-controls')
-    );
+const socket = io();
+const lb = new Lightbar(document.getElementById('light-container'), false);
+const lbc = new TherapistLightbarController(
+    lb,
+    false,
+    socket,
+    document.querySelector('[name="light-width"]'),
+    document.querySelector('[name="light-speed"]'),
+    document.querySelector('[name="light-start"]'),
+    document.getElementById('light-controls')
+);
 
-    new TherapistMethodController(lbc, abc, window.initialSettings);
-})();
+const ab = new Audiobar(document.getElementById('audio-container'), true);
+const abc = new TherapistAudiobarController(
+    ab,
+    true,
+    socket,
+    document.getElementById('audio-pitch-container'),
+    document.querySelector('[name="audio-speed"]'),
+    document.querySelector('[name="audio-start"]'),
+    document.getElementById('audio-controls')
+);
+
+new TherapistMethodController(lbc, abc, window.initialSettings);
--- bin/run_prod.sh
+++ bin/run_prod.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
+yarn install
+npx esbuild app/static/therapist-session.js --bundle --minify --sourcemap --outfile=app/static/dist/therapist-session.min.js 
+npx esbuild app/static/client-session.js --bundle --minify --sourcemap --outfile=app/static/dist/client-session.min.js
+pip install -r requirements.txt
 cd app
 env FLASK_ENV=production uwsgi uwsgi.ini
