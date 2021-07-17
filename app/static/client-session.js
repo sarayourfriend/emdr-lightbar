82889e892eb41f337498eb0badb3d94f98b1e3fc
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-17 14:46:59 -0700

    Turn it into a landing page

--- app/static/client-session.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	const lb = new Lightbar(document.getElementById('light-container'));
-	const ab = new Audiobar(document.getElementById('audio-container'));
-	const lbc = new ClientLightbarController(lb);
-	const abc = new ClientAudiobarController(ab);
-	new ClientMethodController(lbc, abc, window.initialSettings);
-})();
--- bin/run_prod.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-cd app
-env FLASK_ENV=production uwsgi uwsgi.ini
